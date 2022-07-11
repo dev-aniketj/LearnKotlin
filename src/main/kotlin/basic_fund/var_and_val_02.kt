c9b69ba9b88f9c4b3ad1338547f95e7f60635108
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-11 22:11:16 +0530

    master

--- src/main/kotlin/basic_fund/var_and_val_02.kt
+++ src/main/kotlin/basic_fund/var_and_val_02.kt
@@ -1,10 +1,20 @@
 package basic_fund
 
 fun main() {
+    /**
+     * In var we can assign the values
+     */
+
     var name = "Aniket"
-    print("My name is $name")
+    println("My name is $name.")
+
+    name = "Ayush"
+    println("My friends name is $name.")
 
     val age = 22
-    print("My age is " + age)
+    println("My age is " + age)
+
+    //it generates error - bcoz we can't reassign the value on val variable
+    // age = 24
 
 }
