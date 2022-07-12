751d83a0e6dc970bb56f0ad600976c15f7ffd69b
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-12 19:06:19 +0530

    master

---
+++ src/main/kotlin/basic_fund/if_else_10.kt
@@ -0,0 +1,15 @@
+package basic_fund
+
+fun main() {
+    val a = 10
+    val b = 5
+    val c = 12
+
+    if (a > b && a > c) {
+        println("a is greater")
+    } else if (b > c) {
+        println("b is greater")
+    } else {
+        println("c is greater")
+    }
+}
